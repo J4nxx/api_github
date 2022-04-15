9557fd006f8a16ed68768f7afe8aa13e5fd824d7
J4nxx <91869512+J4nxx@users.noreply.github.com>
2022-04-15 13:51:19 +0300

    Delete raamat.py

--- praks2/raamat.py
+++
@@ -1,8 +0,0 @@
-class Raamat():
-   def __init__(self, pealkiri, autor, hind, reiting):
-       self.pealkiri = pealkiri
-       self.autor = autor
-       self.hind = hind
-       self.reiting = reiting
-   def __repr__(self):
-       return self.pealkiri
