bb7fdc139a65b705e904bd30b4acb35951a41c91
J4nxx <91869512+J4nxx@users.noreply.github.com>
2022-04-15 13:51:30 +0300

    Delete restoran.py

--- praks2/restoran.py
+++
@@ -1,9 +0,0 @@
-class Restoraan:
-    def __init__(self,restoraani_nimi, soogi_tyyp):
-        self.restoraani_nimi = restoraani_nimi
-        self.soogi_tyyp = soogi_tyyp
-    def kirjelda_restoraan(self):
-        kirj_tekst = self.restoraani_nimi, 'pakub', self.soogi_tyyp
-        print(str(kirj_tekst))
-    def ava_restoraan(self):
-        print(self.restoraani_nimi, 'on avatud.')
